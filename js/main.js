06f73d2508e07d8a58aab53a7eca031dfd2b3b65
Felix Schäfer <github@jerive.com>
2024-02-29 20:19:58 +0100

    modified:   css/main.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -121,7 +121,7 @@
    $('.project a').magnificPopup({
 
       type:'inline',
-      fixedContentPos: false,
+      fixedContentPos: true,
       removalDelay: 300,
       showCloseBtn: false,
       mainClass: 'mfp-fade'
@@ -132,61 +132,7 @@
    	e.preventDefault();
    	$.magnificPopup.close();
    });
-
 	
-	/*-----------------------------------------------------*/
-  	/* Navigation Menu
-   ------------------------------------------------------ */  
-   var toggleButton = $('.menu-toggle'),
-       nav = $('.main-navigation');
-
-   // toggle button
-   toggleButton.on('click', function(e) {
-
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
-
-	});
-
-   // nav items
-  	nav.find('li a').on("click", function() {   
-
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
-   	// fadeout the navigation panel
-   	nav.fadeOut();   		
-   	     
-  	});
-
-
-   /*---------------------------------------------------- */
-  	/* Highlight the current section in the navigation bar
-  	------------------------------------------------------ */
-	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");	
-
-	sections.waypoint( {
-
-       handler: function(direction) {
-
-		   var active_section;
-
-			active_section = $('section#' + this.element.id);
-
-			if (direction === "up") active_section = active_section.prev();
-
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
-
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		}, 
-
-		offset: '25%'
-	});
-
-
 	/*---------------------------------------------------- */
   	/* Smooth Scrolling
   	------------------------------------------------------ */
@@ -318,3 +264,22 @@ function toggleSkill(skillId) {
         skillItem.classList.remove("active");
     }
 }
+
+// Function to open the modal with the clicked image
+function showModal(clickedImage) {
+	// Get the modal
+	var modal = document.getElementById("imageModal");
+	// Get the modal image
+	var modalImg = document.getElementById("modalImage");
+	// Use the same source as the clicked image for the modal image
+	modalImg.src = clickedImage.src;
+	// Display the modal
+	modal.style.display = "block";
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+	var modal = document.getElementById("imageModal");
+	  modal.style.display = "none";
+  }
+  
